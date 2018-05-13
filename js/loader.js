f720b79b49a294906fca5fcafb2dffe1c8991492
igonglei <gl20061041@hotmail.com>
2018-05-14 00:12:20 +0800

    update

--- js/loader.js
+++ js/loader.js
@@ -2,41 +2,61 @@
  * loader - a jQuery and css3 loading plugin
  * Copyright 2016, Gonglei
  */
-(function($, window, document, undefined) {
+(function(root, factory) {
     "use strict";
+    if (typeof define === "function" && define.amd) {
+        // AMD
+        define(["jquery"], factory);
+    } else if (typeof module === "object" && typeof module.exports === "object") {
+        // CommonJS
+        module.exports = factory(require("jquery"));
+    } else {
+        // Browser globals
+        root.loader = factory(root.jQuery);
+    }
+}(this, function($) {
+    // Document
+    var document = typeof window !== "undefined" ? window.document : {};
     //默认参数
     var defaults = {
-        //动画效果
-        animation: "ball-beat",
-        //颜色
-        color: "#fff",
-        //背景
-        backgroundColor: "rgba(0,0,0,0.6)",
-        //z-index
-        zIndex: 1e4
-    }, //常量以及方法
-    plugin = {
-        //插件名称
-        name: "loader",
-        //动画效果
-        animations: {
-            "ball-beat": 3,
-            "ball-jump": 3,
-            "ball-spin": 8
-        }
-    };
+            //动画效果
+            animation: "ball-beat",
+            //颜色
+            color: "#fff",
+            //背景
+            backgroundColor: "rgba(0,0,0,0.6)",
+            //z-index
+            zIndex: 10000
+        }, //常量以及方法
+        plugin = {
+            //插件名称
+            name: "loader",
+            //动画效果
+            animations: {
+                "ball-beat": 3,
+                "ball-jump": 3,
+                "ball-spin": 8
+            }
+        };
     //构造函数
-    var Loader = function(dom, opts) {
+    var loader = function(dom, opts) {
         this.instanceId = plugin.name + new Date().valueOf();
         this.dom = dom;
         this.options = $.extend({}, defaults, opts);
     };
     //原型
-    Loader.prototype = {
-        constructor: Loader,
+    loader.prototype = {
+        constructor: loader,
         //显示
         show: function() {
-            var $el = $(this.dom), opts = this.options, ani = opts.animation, $mask = $('<div class="' + plugin.name + '" id="' + this.instanceId + '"></div>'), $loader = $("<div></div>"), pos = $el.offset(), $elAni = $('<div class="' + ani + '"></div>'), itemCount = plugin.animations[ani] || 0;
+            var $el = $(this.dom),
+                opts = this.options,
+                ani = opts.animation,
+                $mask = $('<div class="' + plugin.name + '" id="' + this.instanceId + '"></div>'),
+                $loader = $("<div></div>"),
+                pos = $el.offset(),
+                $elAni = $('<div class="' + ani + '"></div>'),
+                itemCount = plugin.animations[ani] || 0;
             $mask.css({
                 display: "none",
                 position: "absolute",
@@ -67,23 +87,23 @@
             $("#" + this.instanceId).remove();
         }
     };
-    window.Loader = Loader;
     //显示加载动画
     $.fn.showLoader = function(opts) {
         return this.hideLoader().each(function() {
-            var loader = new Loader(this, opts);
-            $.data(this, plugin.name, loader);
-            loader.show();
+            var ld = new loader(this, opts);
+            $.data(this, plugin.name, ld);
+            ld.show();
         });
     };
     //隐藏加载动画
     $.fn.hideLoader = function() {
         return this.each(function() {
-            var loader = $.data(this, plugin.name);
-            if (loader) {
-                loader.hide();
+            var ld = $.data(this, plugin.name);
+            if (ld) {
+                ld.hide();
             }
         });
     };
     $.fn.showLoader.defaults = defaults;
-})(jQuery, window, document);
+    return loader;
+}));
